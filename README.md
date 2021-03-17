3c47df438e386d1005c91c5baf1b4e8dc5079d08
Sam <wailam706@gmail.com>
2021-03-17 19:57:52 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Homework-5
+Third-Party APIs Homework: Day Planner
+
+# Objective
+Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+# Requirements
+The app should display standard business hours (9 a.m. to 5 p.m.). Each time slot should represent one hour and contain the following:
+    *The time
+    *A field to hold user input
+    *A save button
+
+# Functionality
+Clicking on the save button will store the time and user input in localStorage.
---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    var keys = Object.keys(localStorage);
+    for (let i = 0; i < keys.length; i++) {
+        var value = localStorage.getItem(keys[i]);
+        var temp = $("#" + keys[i]).find("textarea")
+        temp.val(value);
+    }
+    $(".saveBtn").on("click", function (event) {
+        event.preventDefault();
+        console.log(this);
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, value);
+    });
+    $("#currentDay").text(moment().format("LLL"));
+    function timeFrame() {
+        var currentHours = moment().hours();
+
+        $(".time-block").each(function () {
+            var hourEl = $(this).attr("id");
+            var hourDay = hourEl.substring(5, hourEl.length);
+            var intHourDay = parseInt(hourDay)
+            var intCurrentHours = parseInt(currentHours);
+            if (parseInt(intHourDay) < parseInt(intCurrentHours)) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (parseInt(intHourDay) > parseInt(intCurrentHours)) {
+                $(this).addClass("future");
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+            }
+            else if (parseInt(intHourDay) === parseInt(intCurrentHours)) {
+                $(this).addClass("present");
+                $(this).removeClass("future");
+                $(this).removeClass("past");
+            }
+        })
+    };
+    timeFrame();
+});
